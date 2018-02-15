1aa3464395c2272251e1423c66d8e97f07866718
Aladdin Magdy <aladdinmagdysaber167@commerce.helwan.edu.eg>
2018-02-15 05:23:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,12 +40,11 @@ function total() {
 function removeFromCart(item) {
     for(let i = 0; i < cart.length; i++){
       if(cart[i].hasOwnProperty(item)){
-        cart.splice(i, i + 1);
-      } else {
-        return "That item is not in your cart.";
+        cart.splice(i, 1);
+		return cart;
       }
   }
-  return cart;
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
