b660446cef7a7ceff99ba7db2fa67b984835ef5e
Aladdin Magdy <aladdinmagdysaber167@commerce.helwan.edu.eg>
2018-02-15 05:36:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,5 +51,7 @@ function placeOrder(cardNumber) {
   if(cardNumber){
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     cart = [];
-  } else ""
+  } else {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
 }
